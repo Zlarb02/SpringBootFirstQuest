b3653c878cf859a8141d6519af3d9583f7c70660
Etienne Pogoda <pogoda.etienne@outlook.fr>
2024-06-26 11:35:14 +0200

    first commit

---
+++ src/test/java/com/imthedoctor/who/WhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imthedoctor.who;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
